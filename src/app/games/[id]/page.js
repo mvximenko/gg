afd0a0b0be04cea4009f89aed5968f8265ae1d53
George Maximenko <30684663+mvximenko@users.noreply.github.com>
2023-11-03 15:12:59 +0300

    Add poster and genres

--- src/app/games/[id]/page.js
+++ src/app/games/[id]/page.js
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
-import Rating from './_components/rating';
 import Background from '@/app/_components/background';
 import CustomImage from '@/app/_components/custom-image';
+import IconSearch from '@/assets/icons/search.svg?react';
 import api from '@/api';
 
 export default async function Home({ params }) {
@@ -11,7 +11,7 @@ export default async function Home({ params }) {
     redirect('/404');
   }
 
-  const { name, aggregated_rating, screenshots } = game[0];
+  const { cover, genres, screenshots } = game[0];
 
   return (
     <>
@@ -19,9 +19,26 @@ export default async function Home({ params }) {
         <CustomImage imageId={screenshots?.[0]?.image_id} noContainer />
       </Background>
 
-      <h1 className='text-4xl font-bold'>{name}</h1>
+      <div className='px-8 mb-20 mt-48 grid grid-cols-[0.25fr_1fr] gap-6'>
+        <div>
+          <div className='relative aspect-[3/4] rounded-xl c-bg-dark-blue'>
+            <CustomImage altText='cover' imageId={cover?.image_id} />
+          </div>
 
-      <Rating value={aggregated_rating} />
+          <div className='flex flex-wrap mt-4 gap-2.5 xyz'>
+            {genres?.map((genre) => (
+              <div className='grow rounded-xl text-center p-2' key={genre.id}>
+                {genre.name}
+              </div>
+            ))}
+
+            <div className='font-bold flex items-center justify-center w-full rounded-xl p-3'>
+              <IconSearch className='mr-1' />
+              More like this
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
